f27b2a1e46092bdc9db17d0c4acc7738e742cf9c
jigar2099 <jigar.bhanderi@gmail.com>
2023-05-08 10:49:11 +0200

    update

--- src/tools.py
+++ src/tools.py
@@ -7,29 +7,37 @@ import glob
 from PIL import Image
 from src.surrounding import surrounding
 
-def data_merger(src_dir, label_dir, data_set):
-    train_x = []
-    train_y = []
-    for i in glob.glob(src_dir+"*"+data_set+"*"):
-        src_split1 = i.split('_SRC_')[1]
-        src_split0 = i.split('_SRC_')[0]
-        src_split2 = src_split0.split('/')[-1]        
-        y_path = label_dir+src_split2+'_LABEL_'+src_split1
-        x = np.load(i)
-        y = np.load(y_path)
-        train_x.append(x)
-        train_y.append(y)
-    train_x = np.array(train_x)
-    train_y = np.array(train_y)
-    train_x = train_x.reshape(train_x.shape[0]*train_x.shape[1],train_x.shape[2],train_x.shape[3])
-    train_y = train_y.reshape(train_y.shape[0]*train_y.shape[1],train_y.shape[2],train_y.shape[3])   
-    return train_x, train_y
+def data_merger(dataset_type='train', data_type='src', noise_type='all', dirs = ['noise_7e-3']):
+    X = []
+    for h in dirs:
+        for i in glob.glob('../DATASETS/'):
+            for j in glob.glob(i+'*'):
+                adjusted_j = i+h
+                for k in glob.glob(adjusted_j+'/*'):
+                    adjusted_k = k.split('\ ')[0].split('\\')
+                    if adjusted_k[1]==data_type:
+                        src_path = adjusted_k[0]+'/'+adjusted_k[1]+'/'
+                        for l in glob.glob(src_path+'*'):
+                            if l.split('_')[4] == dataset_type:
+                                all_l = l.split('\ ')[0].split('\\')[0]+'/'+l.split('\ ')[0].split('\\')[1]
+                                if all_l.split('_')[3]==noise_type:
+                                    x = np.load(all_l)
+                                    X.append(x)
+                                elif noise_type == 'all':
+                                    x = np.load(all_l)
+                                    X.append(x)
+                                else: pass
+                            else: pass
+                    else: pass
+    X = np.array(X)
+    X = X.reshape(X.shape[0]*X.shape[1],X.shape[2],X.shape[3])
+    return X
 
 def multiple_data(path_list,type_of_data):
     train_x = []
     train_y = []
     for i in path_list[:]:
-        src_dir = "./src/"+i+"/"; label_dir = "./labels/"+i+"/"
+        src_dir = "/..DATASETS/"+i+'/src/'; label_dir = "/..DATASETS/"+i+'/labels/'
         data_set = type_of_data
         x1, y1 = data_merger(src_dir, label_dir, type_of_data)
         train_x.append(x1)
